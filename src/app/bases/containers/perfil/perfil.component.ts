4fa9f1f057761a1945c9abb196ecbfb782c7a787
vagnerpraia <vagnerpraia@gmail.com>
2018-07-06 19:22:24 -0300

    Adicionando conteúdo ao componente perfil

--- src/app/bases/containers/perfil/perfil.component.ts
+++ src/app/bases/containers/perfil/perfil.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-perfil',
@@ -6,10 +7,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perfil.component.css']
 })
 export class PerfilComponent implements OnInit {
+  public perfil = { id: null, nome: null };
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    this.route.params.subscribe(res => this.perfil.id = res['perfilId']);
   }
 
 }
