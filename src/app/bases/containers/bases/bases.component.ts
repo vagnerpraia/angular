7dd1ed1933b83a8e978ef58885774aa1d2ec5b28
vagnerpraia <vagnerpraia@gmail.com>
2018-07-06 14:48:39 -0300

    Adicionando conteúdo ao componente bases

--- src/app/bases/containers/bases/bases.component.ts
+++ src/app/bases/containers/bases/bases.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-bases',
@@ -6,10 +7,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bases.component.css']
 })
 export class BasesComponent implements OnInit {
+  public posts = [];
 
   constructor() { }
 
   ngOnInit() {
+    for (let i = 1; i < 10; i++) {
+      this.posts.push({ id: i, text: 'This is post with id: ' + i });
+    }
   }
-
 }
